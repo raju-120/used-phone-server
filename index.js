2aaf5fc80c2144af94a20d54543d91b0604cee99
Raju <ashfaqraju27@gmail.com>
2023-07-16 12:10:56 +0600

    user collection saved

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -14,7 +15,7 @@ app.use(express.json());
 
 
 //MongoDB section
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DbUSER}:${process.env.DbPASSWORD}@cluster0.iz2d4md.mongodb.net/?retryWrites=true&w=majority`;
 
 //console.log(uri)
@@ -33,27 +34,69 @@ async function run(){
     try{
         const phoneCollection = client.db('usedPhone').collection('phoneCollections');
         const tabCollection = client.db('usedPhone').collection('tabCollections');
-        const watchCollection = client.db('usedPhone').collection('watchCollections');
+        const smartWatchCollection = client.db('usedPhone').collection('watchCollections');
 
+        //Booking Collection
+        const bookingCollection = client.db('usedPhone').collection('bookings');
+        
+        //Users Collection
+        const emailUserCollection = client.db('usedPhone').collection('emailusers');
+        /* const socialUserCollection = client.db('usedPhone').collection('socialusers');
+ */
+        
 
+        //Phone collection section
         app.get('/phoneCollections', async(req, res) =>{
             const query = {};
-            const phoneCollections = await phoneCollection.find(query).toArray();
-            res.send(phoneCollections);
+            const phones = await phoneCollection.find(query).toArray();
+            res.send(phones);
         });
 
+        app.get('/phoneCollections/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = {  _id : new ObjectId(id) }
+            //console.log(query)
+            const singlePhoneCollection = await phoneCollection.findOne(query);
+            console.log(singlePhoneCollection);
+            res.send(singlePhoneCollection);
+        })
+
+        //Tablet collection section
         app.get('/tabCollections', async (req,res) =>{
             const query = {};
             const tabletCollections= await tabCollection.find(query).toArray();
             res.send(tabletCollections);
         });
 
-        app.get('watchCollections', async (req, res) =>{
+        //watch Collection section
+        app.get('/watchCollections', async (req, res) =>{
             const query = {};
-            const watchCollections = await watchCollection.find(query).toArray();
+            const watchCollections = await smartWatchCollection.find(query).toArray();
             res.send(watchCollections);
         })
 
+        //booking collection section
+        app.post('/bookings', async(req, res) =>{
+            const bookings = req.body;
+            console.log('inside bookings',bookings);
+            const result = await bookingCollection.insertOne(bookings);
+            res.send(result);
+        });
+
+        //store users email
+        app.post('/emailusers', async(req, res) =>{
+            const emailUsers = req.body;
+            const result = await emailUserCollection.insertOne(emailUsers);
+            res.send(result);
+        })
+
+        /* app.post('/socialusers', async(req, res) =>{
+            const socialUser = req.body;
+            const result = await socialUserCollection.insertOne(socialUser);
+            res.send(result);
+        }) */
+
+        
     }
     finally{
 
